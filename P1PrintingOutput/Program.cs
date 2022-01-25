80d2269af49220824cc31a54e8fb60a18a1eb837
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-01-25 16:32:22 +0100

    P1 and P2 done

--- P1PrintingOutput/Program.cs
+++ P1PrintingOutput/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main ()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
---
+++ P2Comments/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Max DK
+ * Tuesday, 25/01/2022
+ * max.deurell@gmail.com
+ */
+namespace Comments
+{
+    class Comments
+    {
+        /// This is the main method, in console it is the method that will run first before all others
+
+        static void Main()
+        {
+            // we are using the console class to use the WriteLine function which takes in a string argument to write out a new line and the string "Hellow World!"
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
