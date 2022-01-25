efeba368a03e729bb105defb74c7b372eb2a1d07
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-01-25 17:03:20 +0100

    P6 assignment done

---
+++ P6Input/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ReadInput
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What´s your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+        }
+    }
+}
+
