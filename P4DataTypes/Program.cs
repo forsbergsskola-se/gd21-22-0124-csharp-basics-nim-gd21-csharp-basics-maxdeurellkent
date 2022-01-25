ba0d9291d1d84e89f30b5a4483cacaf6e729c61a
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-01-25 16:53:47 +0100

    P4 assignment added and done

---
+++ P4DataTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DataTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            string outputString = "Hello World";
+            Console.WriteLine(outputString);
+            float decimalValue = 0.3f;
+            Console.WriteLine(decimalValue);
+            bool isAwesome = true;
+            Console.WriteLine(isAwesome);
+            Byte hexByte = 0xa7;
+            Console.WriteLine(hexByte);
+        }
+    }
+}
