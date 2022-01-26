b4df5a09c976b9d41735a61852a78c95b4c026f4
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-01-26 13:31:58 +0100

    P9 assignment finished

---
+++ P9Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your first name?");
+            string nameFirst = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string nameLast = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello " + nameFirst + " " + nameLast + ", you are " + age + " years old!");
+            
+        }
+    }
+}
+
