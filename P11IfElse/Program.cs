ddc15596b07d5054c5ba49583f61815046838372
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-01-27 15:01:53 +0100

    P11 small change
    replaced unnecessary if else for just else

--- P11IfElse/Program.cs
+++ P11IfElse/Program.cs
@@ -10,7 +10,7 @@
             
             if (ageNum <= 12 & ageNum >= 0) Console.WriteLine("You are a child");
             else if (ageNum <= 19 & ageNum >= 13) Console.WriteLine("You are a teenager");
-            else if (ageNum > 19) Console.WriteLine("You are a grown-up");
+            else Console.WriteLine("You are a grown-up");
 
             Console.WriteLine("Give me another integer.");
             string numString = Console.ReadLine();
@@ -18,12 +18,14 @@
 
             int max = Math.Max(num, ageNum);
             Console.WriteLine("The biggest number out of your age and the number you entered is: " + max);
-
+            
             string oddOrEven = max % 2 != 0 ? "The number you entered is odd" : "The number you entered is even";
             Console.WriteLine(oddOrEven);
-
-            //if (max % 2 != 0) Console.WriteLine("The number you entered is odd");
-            //else if (max % 2 == 0) Console.WriteLine("The number you entered is even");
+            
+            // if (max % 2 != 0) Console.WriteLine("The number you entered is odd");
+            // else Console.WriteLine("The number you entered is even");
+            
+            
 
         }
     }
